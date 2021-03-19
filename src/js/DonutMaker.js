fcf2947d385cda17745a1bf4e792823abc675043
shanfrom90 <shanfrom90@gmail.com>
2021-03-18 21:04:55 -0400

    Added multiplier button and reset button

--- src/js/DonutMaker.js
+++ src/js/DonutMaker.js
@@ -1,12 +1,12 @@
 class Donut {
     constructor() {
-    this.donutCount = 0;
+    this.donutCount = 100;
 
     this.numOfAutoClickers = 0;
     this.autoClickCost = 100;
 
     this.numofMultipliers = 0;
-    this.mulitplierCost = 10;
+    this.multiplierCost = 10;
 
     this.donutsPerClick = Math.pow(1.2, this.numofMultipliers);
     } 
@@ -26,32 +26,49 @@ class Donut {
         return this.autoClickCost;
     }
     increaseAutoClickCost(){
-        this.autoClickCost = this.numOfAutoClickers * .1;
+        this.autoClickCost = this.autoClickCost + this.autoClickCost * .1;
     }
     purchaseAutoClick(){
         if(this.donutCount >= this.autoClickCost){
             this.donutCount = this.donutCount - this.autoClickCost;
-            this.numOfAutoClickers++;
             this.increaseAutoClickCost();
+            this.numOfAutoClickers ++;
+            console.log(this.numOfAutoClickers);
+        }
+        else{
+            alert('You need to make more donuts!');
         }
     }
-
+  
     activateAutoClick() {
         this.donutCount = this.donutCount + this.numOfAutoClickers;
     }
     
-    
 
-    // getMulitplierCount(){
-    //     return this.numofMultipliers;
-    // }
-    // getMulitplierCost(){
-    //     return this.mulitplierCost;
-    // }
-    // //TODO increase multiplier cost, activate multiplier, purchase multiplier
-    // getDonutsPerClick(){
-    //     return this.donutsPerClick;
-    // }
+    getMultiplierCount(){
+        return this.numofMultipliers;
+    }
+
+    getMultiplierCost(){
+        return this.multiplierCost;
+    }
+    purchaseMultiplier(){
+        if(this.donutCount >= this.multiplierCost){
+            this.donutCount = this.donutCount - this.multiplierCost;
+            this.numofMultipliers ++;
+            this.increaseMultiplierCost();
+            this.activateMultiplier();
+        }
+    }
+    activateMultiplier(){
+        this.donutCount = this.donutCount + this.donutCount * .2;
+    }
+    increaseMultiplierCost(){
+        this.multiplierCost = this.multiplierCost + this.multiplierCost * .1;
+    }
+    getDonutsPerClick(){
+        return this.donutsPerClick;
+    }
 
     
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,57 +1,75 @@
 const donutObj = new Donut();
 const donutButton = document.getElementById('donutMaker');
 const donutTotal = document.getElementById('donutTotal');
+const dps = document.getElementById('dps');
 
 const autoClickButton = document.getElementById('autoClicker');
 const autoClickerTotal = document.getElementById('autoTotal');
 const autoClickCost = document.getElementById('autoClickCost');
 
-// const multiButton = document.getElementById('multiplier');
-// const multiTotal = document.getElementById('multiTotal');
-// const multiCost = document.getElementById('multiCost');
+const multiButton = document.getElementById('multiplier');
+const multiTotal = document.getElementById('multiTotal');
+const multiCost = document.getElementById('multiCost');
 
-
-const updateDonutTotal = (donutTotal, donutObj)=>{
-    donutTotal.innerText = donutObj.getDonutNumber();
-}
+const reset = document.getElementById('reset');
 
 const autoClicker = setInterval(autoClick, 1000);
 
  
 function autoClick(){
-    updateDonutTotal(donutTotal, donutObj);
     donutObj.activateAutoClick();
+    UpdateText();
     //something here to eventually turn on the disabled autoclick button (if they don't have enough?)
-    
 }
 
-
-
-const updateAutoClickTotal = (autoClickerTotal, donutObj) => {
-    autoClickerTotal.innerText = Math.round(donutObj.getAutoClickCount());
-}
-
-const updateAutoClickCost = (autoClickCost, donutObj) => {
-    autoClickCost.innerText = Math.round(donutObj.getAutoClickCost())
-}
-
- 
-//not sure if this is right
 
 const makeDonutButton = (donutButton, donutTotal, donutObj) =>{
    donutButton.addEventListener('click', ()=>{
         donutObj.addOneDonut();
-        updateDonutTotal(donutTotal, donutObj);
+        UpdateText();
     })
 } 
 
 const makeAutoClickButton = (autoClickerTotal, autoClickButton, autoClickCost, donutObj)=>{
     autoClickButton.addEventListener('click', ()=>{
         donutObj.purchaseAutoClick();
-        updateAutoClickTotal(autoClickerTotal, donutObj);
-        donutObj.increaseAutoClickCost(autoClickCost, donutObj);
+        UpdateText();
     })
 }
 
+const makeMultiButton = (donutObj, multiCost, multiButton, multiTotal) => {
+    multiButton.addEventListener('click', () =>{
+        donutObj.purchaseMultiplier();
+        console.log(multiTotal.innerText);
+        UpdateText();
+    })
+    }
+
+const makeResetButton = () =>{
+    reset.addEventListener('click',()=>{
+        location.reload();
+    })
+}
+    
+    
 makeDonutButton(donutButton,donutTotal,donutObj);
 makeAutoClickButton(autoClickerTotal, autoClickButton, autoClickCost, donutObj);
+makeMultiButton(donutObj, multiCost, multiButton, multiTotal);
+makeResetButton();
+
+
+function UpdateText() {
+    donutTotal.innerText = "Donuts: " + donutObj.getDonutNumber().toFixed(2);
+
+    autoClickCost.innerText = donutObj.getAutoClickCost().toFixed(2);
+    autoClickerTotal.innerText =  donutObj.getAutoClickCount();
+
+    multiCost.innerText = "Multiplier Cost: " + donutObj.getMultiplierCost().toFixed(2);
+    multiTotal.innerText = "Multiplier: " + donutObj.getMultiplierCount().toFixed(2) + "x Donuts";
+    
+    //dps.innerText = "Donuts Per Second:" + (donutObj.getAutoClickCount() * donutObj.getMultiplierCount());
+
+    } 
+UpdateText(); 
+
+
